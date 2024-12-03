c4e1eabb6b63ceb47571a0d0bc2c7ba8d528f056
John Blake <jcb1354@gmail.com>
2024-12-03 16:27:44 -0500

    Add project files.

---
+++ Minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,298 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public const string HAPPY = "pack://application:,,,/Minesweeper;component/Resources/happy.png";
+        public const string DEAD = "pack://application:,,,/Minesweeper;component/Resources/dead.png";
+        public const string WINNER = "pack://application:,,,/Minesweeper;component/Resources/winner.png";
+        internal MinesweeperBoard board;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void RefreshBoard()
+        {
+            string b = board.DebugToString();
+            //BoardLabel.Content = b;
+            Draw();
+        }
+
+        public void Draw()
+        {
+            Board.Children.Clear();
+            StackPanel rows = new StackPanel();
+            int boardRows = board.Cells.GetLength(0), boardCols = board.Cells.GetLength(1);
+            rows.Name = "Rows";
+            rows.Orientation = Orientation.Vertical;
+            for (int i = 0; i < boardRows; i++)
+            {
+                // Make a stackpanel for each row
+                StackPanel curr_row = new StackPanel();
+                curr_row.Orientation = Orientation.Horizontal;
+                curr_row.Name = "R" + i.ToString();
+
+                for (int j = 0; j < boardCols; j++)
+                {
+                    Button btn = new Button();
+                    BitmapImage tmp_bimg = new BitmapImage();
+                    tmp_bimg.BeginInit();
+                    tmp_bimg.UriSource = GetSrc(board.Cells[i, j]);
+                    tmp_bimg.EndInit();
+                    Image tmp_img = new Image();
+                    tmp_img.Source = tmp_bimg;
+                    btn.Content = tmp_img;
+                    btn.Name = $"x{i}x{j}";
+                    btn.Click += Cell_Click;
+                    curr_row.Children.Add(btn);
+                }
+                rows.Children.Add(curr_row);
+            }
+            Board.Children.Add(rows);
+        }
+
+        public void Cell_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (sender is Button button)
+                {
+                    string cellname = button.Name;
+                    cellname = cellname.Substring(1);
+                    var p = cellname.Split('x');
+                    int r = int.Parse(p[0]);
+                    int c = int.Parse(p[1]);
+
+                    board.GuessCell(r, c);
+
+                    Draw();
+                }
+            }
+            catch (GameOverException goe)
+            {
+                board.RevealAll();
+                Draw();
+                if (goe.Win)
+                {
+                    SetStatus(PlayerStatus.Winner);
+                    StatusLabel.Content = "You won!";
+                }
+                else
+                {
+                    SetStatus(PlayerStatus.Dead);
+                    StatusLabel.Content = "You exploded! Sorry.";
+                }
+            }
+            catch (AlreadyGuessedException) { }
+            catch (Exception c)
+            {
+                MessageBox.Show(c.Message);
+            }
+        }
+        public Uri GetSrc(Minesweeper.MinesweeperCell c)
+        {
+            if (!c.IsRevealed)
+            {
+                return new Uri("pack://application:,,,/Resources/hidden.png");
+            }
+            if (c.IsMine)
+            {
+                return new Uri("pack://application:,,,/Resources/mine.png");
+            }
+            switch (c.NeighborMines)
+            {
+                case 0:
+                    return new Uri("pack://application:,,,/Resources/0.png");
+                case 1:
+                    return new Uri("pack://application:,,,/Resources/1.png");
+                case 2:
+                    return new Uri("pack://application:,,,/Resources/2.png");
+                case 3:
+                    return new Uri("pack://application:,,,/Resources/3.png");
+                case 4:
+                    return new Uri("pack://application:,,,/Resources/4.png");
+                case 5:
+                    return new Uri("pack://application:,,,/Resources/5.png");
+                case 6:
+                    return new Uri("pack://application:,,,/Resources/6.png");
+                case 7:
+                    return new Uri("pack://application:,,,/Resources/7.png");
+                case 8:
+                    return new Uri("pack://application:,,,/Resources/8.png");
+            }
+
+
+            // Should not happen
+            throw new Exception("Image to produce for cell undetermined");
+        }
+        public enum PlayerStatus
+        {
+            Alive = 0,
+            Dead = 1,
+            Winner = 2
+        }
+        public void SetStatus(PlayerStatus status)
+        {
+            switch (status)
+            {
+                case PlayerStatus.Alive:
+                    AliveImg.Visibility = Visibility.Visible;
+                    DeadImg.Visibility = Visibility.Collapsed;
+                    WinnerImg.Visibility = Visibility.Collapsed;
+                    break;
+                case PlayerStatus.Dead:
+                    AliveImg.Visibility = Visibility.Collapsed;
+                    DeadImg.Visibility = Visibility.Visible;
+                    WinnerImg.Visibility = Visibility.Collapsed;
+                    break;
+                case PlayerStatus.Winner:
+                    AliveImg.Visibility = Visibility.Collapsed;
+                    DeadImg.Visibility = Visibility.Collapsed;
+                    WinnerImg.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Opens New Game Selection Box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewGameBox.Visibility == Visibility.Visible)
+                NewGameBox.Visibility = Visibility.Collapsed;
+            else
+                NewGameBox.Visibility = Visibility.Visible;
+        }
+        /// <summary>
+        /// User submits choices for starting new game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewGameSubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Get difficulty
+            var choice = DifficultyRadioBtns.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked.HasValue && r.IsChecked.Value);
+            if (choice == null)
+            {
+                MessageBox.Show("You must select a difficulty.");
+                return;
+            }
+            MinesweeperDifficulty d;
+            switch (choice.Content.ToString())
+            {
+                case "Easy":
+                    d = MinesweeperDifficulty.EASY;
+                    break;
+                case "Medium":
+                    d = MinesweeperDifficulty.MEDIUM;
+                    break;
+                case "Hard":
+                    d = MinesweeperDifficulty.HARD;
+                    break;
+                default:
+                    MessageBox.Show($"Unknown difficulty selection: {choice.Content.ToString()}");
+                    return;
+            }
+
+            // Get Board Size
+            string rowText = NewBoardRowInput.Text;
+            string colText = NewBoardColumnInput.Text;
+            if (!int.TryParse(rowText, out int rows))
+            {
+                MessageBox.Show($"Invalid row amount: \"{rowText}\"\nMust be a positive number.");
+                return;
+            }
+            if (!int.TryParse(colText, out int cols))
+            {
+                MessageBox.Show($"Invalid column amount: \"{colText}\"\nMust be a positive number.");
+                return;
+            }
+            
+
+            try
+            {
+                board = new MinesweeperBoard(rows, cols, d);
+                // Successfully creating board should close the new game box
+                NewGameBox.Visibility = Visibility.Collapsed;
+                MinesLabel.Content = $"Mines: {board.MineCount}";
+                DifficultyLabel.Content = $"Difficulty: {Minesweeper.Utils.StringParse(board.Difficulty)}";
+                SetStatus(PlayerStatus.Alive);
+                StatusLabel.Content = "";
+                RefreshBoard();
+            }
+            catch (Exception g)
+            {
+                MessageBox.Show(g.Message);
+            }
+        }
+
+        /// <summary>
+        /// Debug control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /*
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (board is null) return;
+            if (board.IsGameOver) return;
+
+            string rowText = RowField.Text;
+            string colText = ColumnField.Text;
+            if (!int.TryParse(rowText, out int row))
+            {
+                MessageBox.Show($"Invalid row amount: \"{rowText}\"\nMust be a positive number.");
+                return;
+            }
+            if (!int.TryParse(colText, out int col))
+            {
+                MessageBox.Show($"Invalid column amount: \"{colText}\"\nMust be a positive number.");
+                return;
+            }
+
+            try
+            {
+                board.GuessCell(row, col);
+                // A safe cell was guessed if no exception thrown
+
+            }
+            catch (Minesweeper.GameOverException g)
+            {
+                if (g.Win)
+                {
+                    SetStatus(PlayerStatus.Winner);
+                    MessageBox.Show("You Won!");
+                }
+                else
+                {
+                    SetStatus(PlayerStatus.Dead);
+                    MessageBox.Show("You Lost!");
+                }
+            }
+            catch (Exception g)
+            {
+                MessageBox.Show(g.Message);
+            }
+            RefreshBoard();
+        }
+        //*/
+    }
+}
---
+++ TestingPage/MinesweeperBoard.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class GameOverException : Exception
+    {
+        public bool Win;
+        public GameOverException(bool win = false) : base() { Win = win; }
+        public GameOverException(string message, bool win = false) : base() { Win = win; }
+    }
+    public class RNG
+    {
+        static Random _rng = new Random();
+        /// <summary>
+        /// Returns random float value from 0.0 to 1.0
+        /// </summary>
+        /// <returns></returns>
+        public static float RandFloat()
+        {
+            return float.Parse("" + Math.Round(_rng.NextDouble(), 3));
+        }
+    }
+    public enum MinesweeperDifficulty
+    {
+        EASY = 1,
+        MEDIUM = 2,
+        HARD = 3
+    }
+    public class Utils
+    {
+        public static string StringParse(MinesweeperDifficulty s)
+        {
+            foreach (var b in Enum.GetValues(typeof(MinesweeperDifficulty)))
+            {
+                if ((int)s == (int)b)
+                {
+                    return Enum.GetName(typeof(MinesweeperDifficulty), b);
+                }
+            }
+            return "";
+        }
+    }
+    public class MinesweeperCell
+    {
+        private bool _isMine;
+        private bool _isFlagged;
+        private bool _isRevealed;
+        private int _neighborMines;
+
+        public MinesweeperCell(bool isMine)
+        {
+            _isRevealed = false;
+            _isMine = isMine;
+        }
+        public bool IsMine => _isMine;
+        public bool IsFlagged => _isFlagged;
+        public bool IsRevealed => _isRevealed;
+        public int NeighborMines => _neighborMines;
+        public void Reveal()
+        {
+            _isRevealed = true;
+        }
+        public void SetNeighborMines(int n)
+        {
+            if (0 <= n && n <= 8)
+            {
+                _neighborMines = n;
+            }
+        }
+    }
+    public class MinesweeperBoard
+    {
+        private static int MIN_SIZE = 4;
+        public MinesweeperCell[,] Cells;
+
+        private int _cellsRemaining;
+        private int _mineCount;
+        private bool _isGameOver;
+        public bool IsGameOver => _isGameOver;
+        public int MineCount => _mineCount;
+        private MinesweeperDifficulty _diff;
+        public MinesweeperDifficulty Difficulty => _diff;
+
+        public MinesweeperBoard(int boardRows, int boardCols, MinesweeperDifficulty difficulty)
+        {
+            if (boardRows < MIN_SIZE || boardCols < MIN_SIZE)
+            {
+                throw new ArgumentOutOfRangeException($"Error creating MinesweeperBoard:\nNumber of rows {boardRows} and number of columns {boardCols} must both be at least {MIN_SIZE}.");
+            }
+
+            float mineChance = 0.0f;
+            switch (difficulty)
+            {
+                case MinesweeperDifficulty.EASY:
+                    mineChance = 0.1f;
+                    break;
+                case MinesweeperDifficulty.MEDIUM:
+                    mineChance = 0.2f;
+                    break;
+                case MinesweeperDifficulty.HARD:
+                    mineChance = 0.3f;
+                    break;
+            }
+
+            _diff = difficulty;
+
+            // Randomly place mines on the board.
+            int mineCount = 0;
+            // Loop ensures that a board with at least one mine is generated.
+            while (mineCount == 0)
+            {
+                Cells = new MinesweeperCell[boardRows, boardCols];
+                for (int i = 0; i < boardRows; i++)
+                {
+                    for (int j = 0; j < boardCols; j++)
+                    {
+                        if (RNG.RandFloat() < mineChance)
+                        {
+                            Cells[i, j] = new MinesweeperCell(true);
+                            mineCount++;
+                        }
+                        else
+                        {
+                            Cells[i, j] = new MinesweeperCell(false);
+                        }
+                    }
+                }
+            }
+            
+            _mineCount = mineCount;
+            _cellsRemaining = boardRows * boardCols - mineCount; // The number of non-mine cells.
+            _isGameOver = false;
+
+            // Assign number of mines around each cell to each cell's data
+            for (int i = 0; i < boardRows; i++)
+            {
+                for (int j = 0; j < boardCols; j++)
+                {
+                    Cells[i, j].SetNeighborMines(GetMinesAroundCell(i, j));
+                }
+            }
+        }
+        public void Draw(bool debug = false)
+        {
+            if (debug)
+            {
+                Console.WriteLine(DebugToString());
+            }
+        }
+        public string DebugToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Cells.GetLength(0); i++)
+            {
+                for (int c = 0; c < Cells.GetLength(1); c++)
+                {
+                    if (Cells[i, c].IsRevealed)
+                    {
+                        if (Cells[i, c].IsMine)
+                        {
+                            sb.Append("F");
+                        }
+                        else
+                        {
+                            sb.Append(Cells[i, c].NeighborMines + "");
+                        }
+                    }
+                    else sb.Append("?");
+                }
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+        private int GetMinesAroundCell(int row, int col)
+        {
+            int mines = 0;
+            for (int n = row - 1; n <= row + 1; n++)
+            {
+                /*  X--     Checks left of cell
+                 *  X--
+                 *  X--
+                 */ 
+                try
+                {
+                    if (Cells[n, col - 1].IsMine)
+                    {
+                        mines++;
+                    }
+                }
+                catch (Exception) { }
+                /*  --X     Checks right of cell
+                 *  --X
+                 *  --X
+                 */
+                try
+                {
+                    if (Cells[n, col + 1].IsMine)
+                    {
+                        mines++;
+                    }
+                }
+                catch (Exception) { }
+            }
+            for (int n = row - 1; n <= row + 1; n += 2)
+            {
+                /*  -X-     Checks below and above cell
+                 *  ---
+                 *  -X-
+                 */
+                try
+                {
+                    if (Cells[n, col].IsMine)
+                    {
+                        mines++;
+                    }
+                }
+                catch (Exception) { }
+            }
+            return mines;
+        }
+        /// <summary>
+        /// Returns true if cell is not a mine, false if mine (game over).
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public bool GuessCell(int row, int col)
+        {
+            if (Cells[row, col].IsRevealed)
+            {
+                throw new Exception("That cell is already revealed.");
+            }
+            if (Cells[row, col].IsMine)
+            {
+                // Game over!
+                this.RevealAll();
+                _isGameOver = true;
+                throw new GameOverException(win: false);
+            }
+            this.RecursiveReveal(row, col, Cells[row, col].NeighborMines);
+            CheckForWin();
+            return true;
+        }
+        public void CheckForWin()
+        {
+            if (_cellsRemaining == 0)
+            {
+                this.RevealAll();
+                _isGameOver = true;
+                throw new GameOverException(win: true);
+            }
+        }
+        public void RecursiveReveal(int r, int c, int mineCount)
+        {
+            if (Cells[r, c].IsMine) return;
+            if (!Cells[r, c].IsRevealed)
+            {
+                Cells[r, c].Reveal();
+                _cellsRemaining--;
+            }
+            
+            // Left
+            try
+            {
+                if (!Cells[r, c - 1].IsRevealed && Cells[r, c - 1].NeighborMines == mineCount)
+                {
+                    RecursiveReveal(r, c - 1, mineCount);
+                }
+            }
+            catch { }
+
+            // Up
+            try
+            {
+                if (!Cells[r - 1, c].IsRevealed && Cells[r - 1, c].NeighborMines == mineCount)
+                {
+                    RecursiveReveal(r - 1, c, mineCount);
+                }
+            }
+            catch { }
+
+            // Down
+            try
+            {
+                if (!Cells[r + 1, c].IsRevealed && Cells[r + 1, c].NeighborMines == mineCount)
+                {
+                    RecursiveReveal(r + 1, c, mineCount);
+                }
+            }
+            catch { }
+
+            // Right
+            try
+            {
+                if (!Cells[r, c + 1].IsRevealed && Cells[r, c + 1].NeighborMines == mineCount)
+                {
+                    RecursiveReveal(r, c + 1, mineCount);
+                }
+            }
+            catch { }
+        }
+        public void RevealAll()
+        {
+            foreach (MinesweeperCell cell in Cells)
+            {
+                cell.Reveal();
+            }
+        }
+    }
+}
